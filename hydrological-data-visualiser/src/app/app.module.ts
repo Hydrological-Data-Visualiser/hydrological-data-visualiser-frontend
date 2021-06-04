6e708749fa2b89b702b84983c5d506eeb18a052d
Jan <jan.chyb.99@gmail.com>
2021-06-04 12:41:16 +0200

    Change navbar, add modal and resize map

--- hydrological-data-visualiser/src/app/app.module.ts
+++ hydrological-data-visualiser/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormModalComponent } from './components/form-modal/form-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
-    NavbarComponent
+    NavbarComponent,
+    FormModalComponent
   ],
   imports: [
     BrowserModule,
