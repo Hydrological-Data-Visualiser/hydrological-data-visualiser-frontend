f5b22188696ae466168adee1d4f24d60f6ab0f9c
Wiktor Tarsa <wiktor.tarsa@gmail.com>
2021-06-04 11:32:30 +0200

    add navbar component template

--- hydrological-data-visualiser/src/app/app.module.ts
+++ hydrological-data-visualiser/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
