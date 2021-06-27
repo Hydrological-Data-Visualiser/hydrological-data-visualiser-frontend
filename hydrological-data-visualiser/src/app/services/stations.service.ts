6ca4d7f4d10d0072eec4d39ada78cf2fc3e78014
Przemysław Lechowicz <lechowiczprzemyslaw@gmail.com>
2021-06-27 18:59:06 +0200

    Code reformat

--- hydrological-data-visualiser/src/app/services/stations.service.ts
+++ hydrological-data-visualiser/src/app/services/stations.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {Station} from '../model/Station';
 import * as L from 'leaflet';
-import {BehaviorSubject} from 'rxjs';
+import {Subject} from 'rxjs';
 import 'leaflet.markercluster';
 
 @Injectable({
@@ -10,24 +10,16 @@ import 'leaflet.markercluster';
 })
 export class StationsService {
   private stationFilePath = '/assets/data/wykaz_stacji.csv';
-  public stationList: Station[] = [];
   public map: any;
-  private clickedMarker = new BehaviorSubject<Station | undefined>(undefined);
+  private clickedMarker = new Subject<Station>();
   public clickedMarker$ = this.clickedMarker.asObservable();
   public group = new L.MarkerClusterGroup({
-    spiderfyOnMaxZoom: true,
     showCoverageOnHover: false,
-    zoomToBoundsOnClick: true
   });
-  private stations = new BehaviorSubject<Station | undefined>(undefined);
+  private stations = new Subject<Station>();
   public stations$ = this.stations.asObservable();
 
   constructor(private http: HttpClient) {
-    this.stations$.subscribe(a => {
-      if (a) {
-        this.stationList.push(a);
-      }
-    });
     this.getDataRecordsArrayFromCSVFile();
   }
 
@@ -52,50 +44,51 @@ export class StationsService {
     } = {
       responseType: 'arraybuffer'
     };
-    const tab: Station[] = [];
-    const a = this.http.get(this.stationFilePath, options);
-    a.subscribe((res) => {
-      const enc = new TextDecoder('utf-8');
+    this.http.get(this.stationFilePath, options)
+      .subscribe((res) => {
+        const enc = new TextDecoder('utf-8');
 
-      (enc.decode(res).split('\n').forEach(e => {
-        let latitude: number | undefined;
-        let longitude: number | undefined;
-        let name: string | undefined;
-        let id: number | undefined;
-        let geoId: number | undefined;
-        let station: Station | undefined;
-        if (e.split(',')[0]) {
-          id = parseInt(e.split(',')[0], 10);
-        }
-        if (e.split(',')[1]) {
-          name = this.capitalize(e.split(',')[1]);
-        }
-        if (e.split(',')[2]) {
-          geoId = parseInt(e.split(',')[2], 10);
-        }
-        if (e.split(',')[3]) {
-          latitude = parseFloat(e.split(',')[3]);
-        }
-        if (e.split(',')[4]) {
-          longitude = parseFloat(e.split(',')[4]);
-        }
-        if (id && name && geoId) {
-          station = new Station(id, name, geoId, latitude, longitude);
-          tab.push(station);
-        }
-        if (id && name && geoId && longitude && latitude) {
-          const marker = L.marker(new L.LatLng(latitude, longitude), {icon: this.redIcon}).on('click', event => {
-            this.clickedMarker.next(station);
-          }).bindPopup(name);
-          this.stations.next(station);
-          this.group.addLayer(marker);
-          this.map.addLayer(this.group);
-        }
-      }));
-    });
+        (enc.decode(res).split('\n').forEach(e => {
+          let latitude: number | undefined;
+          let longitude: number | undefined;
+          let name: string | undefined;
+          let id: number | undefined;
+          let geoId: number | undefined;
+          if (e.split(',')[0]) {
+            id = parseInt(e.split(',')[0], 10);
+          }
+          if (e.split(',')[1]) {
+            name = this.capitalize(e.split(',')[1]);
+          }
+          if (e.split(',')[2]) {
+            geoId = parseInt(e.split(',')[2], 10);
+          }
+          if (e.split(',')[3]) {
+            latitude = parseFloat(e.split(',')[3]);
+          }
+          if (e.split(',')[4]) {
+            longitude = parseFloat(e.split(',')[4]);
+          }
+          if (id && name && geoId) {
+            const station = new Station(id, name, geoId, latitude, longitude);
+            this.stations.next(station);
+            this.createMarker(station);
+          }
+        }));
+      });
   }
 
   capitalize(s: string): string {
     return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
+  }
+
+  createMarker(station: Station): void {
+    if (station.longitude && station.latitude) {
+      const marker = L.marker(new L.LatLng(station.latitude, station.longitude), {icon: this.redIcon}).on('click', event => {
+        this.clickedMarker.next(station);
+      }).bindPopup(station.name);
+      this.group.addLayer(marker);
+      this.map.addLayer(this.group);
+    }
   }
 }
