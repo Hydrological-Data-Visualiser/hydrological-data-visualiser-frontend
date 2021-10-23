1b24cccdcc6f56740754979c92486c0fe00b0c68
przemq20 <lechowiczprzemyslaw@gmail.com>
2021-10-23 14:33:38 +0200

    INZ-37-Determination_of_endpoints_according_to_swager

--- hydrological-data-visualiser/src/app/services/river.service.ts
+++ hydrological-data-visualiser/src/app/services/river.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import * as L from 'leaflet';
+import {RiverPoint} from '../model/river-point';
+import {LatLng} from 'leaflet';
 
 @Injectable({
   providedIn: 'root'
@@ -14,11 +16,12 @@ export class RiverService {
 
   showKocinkaRiver(map: L.Map): void {
     if (status) {
-      this.http.get(this.kocinka).subscribe((res: any) => {
-        const cords = res.features[0].geometry.coordinates;
-        for (let i = 0; i < cords.length - 1; i++) {
-          const table = [[cords[i][1], cords[i][0]], [cords[i + 1][1], cords[i + 1][0]]];
-          const color = this.getColor((Number(cords[i][2]) + Number(cords[i + 1][2])) / 2);
+      this.http.get<RiverPoint[]>('https://imgw-mock.herokuapp.com/kocinka/data').subscribe((res: RiverPoint[]) => {
+        for (let i = 0; i < res.length - 1; i++) {
+          const table: LatLng[] = [];
+          table.push(new LatLng(res[i].latitude, res[i].longitude));
+          table.push(new LatLng(res[i + 1].latitude, res[i + 1].longitude));
+          const color = this.getColor((Number(res[i].value) + Number(res[i + 1].value)) / 2);
           L.polyline(table, {color}).addTo(map);
         }
       });
@@ -26,13 +29,13 @@ export class RiverService {
   }
 
   getColor(d: number): string {
-    return d === 1 ? '#DEEDCF' :
-      d === 1.5 ? '#BFE1B0' :
-        d === 2 ? '#99D492' :
-          d === 2.5 ? '#56B870' :
-            d === 3 ? '#1D9A6C' :
-              d === 3.5 ? '#137177' :
-                d === 4 ? '#0A2F51' :
+    return d < 1 ? '#DEEDCF' :
+      d < 1.5 ? '#BFE1B0' :
+        d < 2 ? '#99D492' :
+          d < 2.5 ? '#56B870' :
+            d < 3 ? '#1D9A6C' :
+              d < 3.5 ? '#137177' :
+                d < 4 ? '#0A2F51' :
                   '#CCEDFF';
   }
 }
