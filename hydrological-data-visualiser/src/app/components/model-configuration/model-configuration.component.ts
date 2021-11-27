113a71585f14682f4b98f771f3c9aac1a65b09ab
Wiktor Tarsa <wiktor.tarsa@gmail.com>
2021-11-27 14:38:01 +0100

    add url validation

--- hydrological-data-visualiser/src/app/components/model-configuration/model-configuration.component.ts
+++ hydrological-data-visualiser/src/app/components/model-configuration/model-configuration.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {ApplicationRef, Component, OnInit} from '@angular/core';
 import {DataProviderService} from '../../services/data-provider.service';
 import {DataType} from '../../model/data-type';
 import {DataModelBase} from '../../model/data-model-base';
 import {HttpClient} from '@angular/common/http';
-import {concatMap} from 'rxjs/operators';
-import {of, pipe} from 'rxjs';
 
 @Component({
   selector: 'app-model-configuration',
@@ -12,8 +10,8 @@ import {of, pipe} from 'rxjs';
   styleUrls: ['./model-configuration.component.css']
 })
 export class ModelConfigurationComponent implements OnInit {
-
   url = '';
+  urlAdded = false;
   public dataModelBase!: DataModelBase;
   constructor(private dataProvider: DataProviderService, public http: HttpClient) { }
 
@@ -21,28 +19,36 @@ export class ModelConfigurationComponent implements OnInit {
   }
 
   onSubmit(): void {
-    this.dataProvider.apis.push(this.url);
-    this.http.get<DataModelBase>(`${this.url}/info`).subscribe(data => {
-      switch (data.dataType) {
-        case DataType.LINE: {
-          this.dataProvider.getUniversalLineService().setUrl(this.url);
-          break;
+    this.urlAdded = false;
+    this.checkIfUrlAlreadyAdded();
+    if (!this.urlAdded) {
+      this.dataProvider.apis.push(this.url);
+      this.http.get<DataModelBase>(`${this.url}/info`).subscribe(data => {
+        switch (data.dataType) {
+          case DataType.LINE: {
+            this.dataProvider.getUniversalLineService().setUrl(this.url);
+            break;
+          }
+          case DataType.POINTS: {
+            this.dataProvider.getUniversalMarkerCreatorService().setUrl(this.url);
+            break;
+          }
+          case DataType.POLYGON: {
+            this.dataProvider.getUniversalPolygonsService().setUrl(this.url);
+            break;
+          }
         }
-        case DataType.POINTS: {
-          this.dataProvider.getUniversalMarkerCreatorService().setUrl(this.url);
-          break;
-        }
-        case DataType.POLYGON: {
-          this.dataProvider.getUniversalPolygonsService().setUrl(this.url);
-          break;
-        }
-      }
-    }, error => {
-      console.log('cannot get ' + this.url + '/info.');
-      this.dataProvider.apis.pop();
-    });
-    this.dataProvider.getModels();
-    // @ts-ignore -
-    document.getElementById('dismissButtonModalLabel').click();
+      }, error => {
+        console.log('cannot get ' + this.url + '/info.');
+        this.dataProvider.apis.pop();
+      });
+      this.dataProvider.getModels();
+      // @ts-ignore -
+      document.getElementById('dismissButtonModalLabel').click();
+    }
+  }
+
+  checkIfUrlAlreadyAdded(): void {
+    this.dataProvider.apis.forEach(api => this.urlAdded = this.urlAdded || (api === this.url));
   }
 }
