5731be89ca3936f414e5ef3d8860c0eb9e76a2db
przemq20 <lechowiczprzemyslaw@gmail.com>
2021-12-09 22:17:36 +0100

    Make stations button disabled, add snackBar when clicked on disabled button

--- hydrological-data-visualiser/src/app/components/navbar/navbar.component.ts
+++ hydrological-data-visualiser/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {DataProviderService} from '../../services/data-provider.service';
+import {SidePanelService} from '../side-panel/side-panel-service';
+import {DataType} from '../../model/data-type';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-navbar',
@@ -6,12 +10,26 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  visibleStations = false;
 
-  constructor() {
+  constructor(private dataProvider: DataProviderService, private sidePanelService: SidePanelService, private snackBar: MatSnackBar) {
   }
 
   ngOnInit(): void {
     // @ts-ignore
     document.getElementById('modelsButton').click();
+
+    this.sidePanelService.modelEmitter.subscribe(model => {
+      this.visibleStations = this.dataProvider.getActualService().info.dataType === DataType.POINTS;
+    });
+  }
+
+  openSnackBar(): void {
+    this.snackBar.open('There are no stations in this dataModel - select another with "Points" dataType', 'Ok', {
+      duration: 5000,
+      panelClass: ['mat-toolbar', 'mat-primary']
+    }).onAction().subscribe(() =>
+      // @ts-ignore
+      document.getElementById('modelsButton').click());
   }
 }
